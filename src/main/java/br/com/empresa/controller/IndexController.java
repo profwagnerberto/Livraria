40998fcb693c94ac8fb3d3582461be553e0ee75d
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-07-01 15:27:16 -0300

    Index controler and html, created.

---
+++ src/main/java/br/com/empresa/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.com.empresa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+  
+  @GetMapping("/")
+  public ModelAndView getIndex() {
+    
+    ModelAndView mv = new ModelAndView("index");
+    
+    return mv;
+  }
+
+}
